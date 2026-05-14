bbc83ef6e8c810640062e1a1a0e5eb18229ebcf9
NatsuCamellia <dev@natsucamellia.net>
2026-05-14 23:46:29 +0800

    feat: auto-completion for dotfiles and tmuxinator

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -1,7 +1,11 @@
 alias dot='git --git-dir=$HOME/.dotfiles/dotfiles.git --work-tree=$HOME'
+compdef dot=git
 alias la='ls -A'
 alias ll='ls -alF'
 alias enve='$EDITOR ~/.zshenv'
 alias lenve='$EDITOR ~/.zshenv.local'
-alias confe='$EDITOR $ZDOTDIR'
+alias confe='$EDITOR ~/.config'
+alias tm='tmux'
+alias tn='tmuxinator'
+compdef tn=tmuxinator
 
--- .zshenv
+++ .zshenv
@@ -2,6 +2,8 @@
 ZDOTDIR=$HOME/.config/zsh
 
 export XDG_CONFIG_HOME="$HOME/.config"
+# auto-completion
+export fpath=($fpath $HOME/.config/zsh/functions)
 
 export PATH=~/.local/bin:$PATH
 
