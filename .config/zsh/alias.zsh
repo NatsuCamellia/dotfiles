049124b38efde7df037b0e11bdfacd8abf1c260c
NatsuCamellia <dev@natsucamellia.net>
2026-05-16 12:50:39 +0800

    fix: guard tmuxinator alias with command check
    Also remove tm alias since it's not used often.

--- .config/zsh/alias.zsh
+++ .config/zsh/alias.zsh
@@ -5,7 +5,8 @@ alias ll='ls -alF'
 alias enve='$EDITOR ~/.zshenv'
 alias lenve='$EDITOR ~/.zshenv.local'
 alias confe='$EDITOR ~/.config'
-alias tm='tmux'
-alias tn='tmuxinator'
-compdef tn=tmuxinator
 
+if (( $+commands[tmuxinator] )); then
+    alias tn='tmuxinator'
+    compdef tn=tmuxinator
+fi
