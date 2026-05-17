69899419203a7c5de269a9c0fe1c3cf26929aac0
NatsuCamellia <dev@natsucamellia.net>
2026-05-17 16:53:23 +0800

    nvim: add Gitsigns keymaps

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -32,10 +32,62 @@ return { -- Adds git related signs to the gutter, as well as utilities for manag
     },
   },
   config = function()
+    local gitsigns = require('gitsigns')
     vim.keymap.set("n", "<leader>gb", require("gitsigns").blame, { desc = "[G]itsign [B]lame" })
     vim.keymap.set("n", "<leader>gc", require("gitsigns").show_commit, { desc = "[G]itsign Show [C]ommit" })
     vim.keymap.set("n", "<leader>gtb", require("gitsigns").toggle_current_line_blame, { desc = "[G]itsign [T]oggle [B]lame" })
     vim.keymap.set("n", "<leader>gh", require("gitsigns").stage_hunk, { desc = "[G]itsign Stage [H]unk" })
     vim.keymap.set("n", "<leader>gs", require("gitsigns").show, { desc = "[G]itsign [S]how" })
+
+    -- Navigation
+    vim.keymap.set('n', ']c', function()
+      if vim.wo.diff then
+        vim.cmd.normal({']c', bang = true})
+      else
+        gitsigns.nav_hunk('next')
+      end
+    end)
+
+    vim.keymap.set('n', '[c', function()
+      if vim.wo.diff then
+        vim.cmd.normal({'[c', bang = true})
+      else
+        gitsigns.nav_hunk('prev')
+      end
+    end)
+
+    -- Actions
+    vim.keymap.set('n', '<leader>hs', gitsigns.stage_hunk, { desc = "Gitsigns: Stage Hunk" })
+    vim.keymap.set('n', '<leader>hr', gitsigns.reset_hunk, { desc = "Gitsigns: Reset Hunk" })
+
+    vim.keymap.set('v', '<leader>hs', function()
+      gitsigns.stage_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+    end, { desc = "Gitsigns: Stage Selected Hunk" })
+
+    vim.keymap.set('v', '<leader>hr', function()
+      gitsigns.reset_hunk({ vim.fn.line('.'), vim.fn.line('v') })
+    end, { desc = "Gitsigns: Reset Selected Hunk" })
+
+    vim.keymap.set('n', '<leader>hS', gitsigns.stage_buffer, { desc = "Gitsigns: Stage Buffer" })
+    vim.keymap.set('n', '<leader>hR', gitsigns.reset_buffer, { desc = "Gitsigns: Reset Buffer" })
+    vim.keymap.set('n', '<leader>hp', gitsigns.preview_hunk, { desc = "Gitsigns: Preview Hunk" })
+    vim.keymap.set('n', '<leader>hi', gitsigns.preview_hunk_inline, { desc = "Gitsigns: Preview Hunk Inline" })
+
+    vim.keymap.set('n', '<leader>hb', function()
+      gitsigns.blame_line({ full = true })
+    end, { desc = "Gitsigns: Blame Line" })
+
+    vim.keymap.set('n', '<leader>hd', gitsigns.diffthis, { desc = "Gitsigns: Diff This" })
+
+    vim.keymap.set('n', '<leader>hD', function()
+      gitsigns.diffthis('~')
+    end, { desc = "Gitsigns: Diff This ~" })
+
+    vim.keymap.set('n', '<leader>hq', gitsigns.setqflist, { desc = "Gitsigns: Quickfix List"})
+    vim.keymap.set('n', '<leader>hQ', function() gitsigns.setqflist('all') end, { desc = "Gitsigns: Quickfix List (All)" })
+
+    -- Toggles
+    vim.keymap.set('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = "Gitsigns: Toggle Line Blame"})
+    vim.keymap.set('n', '<leader>tw', gitsigns.toggle_word_diff, { desc = "Gitsigns: Toggle Word Diff"})
   end,
 }
